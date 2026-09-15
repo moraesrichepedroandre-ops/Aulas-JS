c6de84974d6819a63f0440827efa37552364f8d3
André <moraesrichepedroandre@gmail.com>
2026-09-15 11:39:19 -0300

    adicionada pasta 15-09 - variaveis

---
+++ 15-09/script.js
@@ -0,0 +1,45 @@
+// let miranha = "Peter Parker";
+// let numero = 99999999;
+// let numero2 = "999999999";
+// let idade = 17;
+// var nome =  "odirlei";
+// nome = "pedro";
+
+// const nome = "odirlei";
+// nome = "pedro";
+
+//idade = 20;
+//idade = 21;
+
+// console.log("o miranha é o " + miranha + "e ele tem " + idade + " anos");
+// Crie 4 variaveis contendo: nome, sobrenome, idade e curso
+// Exiba no console em  uma unica frase
+
+let nome = "André";
+let sobrenome = "Moraes Riche";
+let idade = 17;
+let curso = "TI";
+
+nome = "André";
+sobrenome = "Moraes Riche";
+idade = 17;
+curso = "TI";
+
+// console.log("Meu nome é " + nome + "meu sobrenome é " + sobrenome + " tenho " + idade + "eu curso" + curso)
+
+// let nome = "André"; //string
+// let peso = 65; //int
+// let altura = 1.82; //float
+// let funcionario = false ; //bool - true == verdadeiro ou false == falso
+
+// let cidade = null;
+// let estado;
+// console.log(typeof nome);
+// console.log(typeof peso);
+// console.log(typeof altura);
+// console.log(typeof funcionario)
+
+console.log("A variavel nome tem " + nome + "e é do tipo" + typeof nome )
+console.log("A variavel sobrenome tem " + sobrenome + "e é do tipo" + typeof sobrenome )
+console.log("A variavel idade tem " + idade + "e é do tipo" + typeof idade )
+console.log("A variavel curso tem " + curso + "e é do tipo" + typeof curso )
